a0dc283a809ead321778e1297b05ccd329eebcee
zengxuan <zeng.xu@northeastern.edu>
2022-01-27 01:01:47 -0800

    added a note

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,6 +15,8 @@ export default function SearchBar() {
                     navigate('./searchResult/' + searchContent);
                 }}>SEARCH</button>
             </div>
+            <div id='note'>Dear visitor --  in order to update or delete a job, you have to be its creator.
+                You can try posting a job, or login with username: zengxuan/password: zengxuan to update or delete the job titled 'Sales Engineer'. </div>
         </>
     );
 }
