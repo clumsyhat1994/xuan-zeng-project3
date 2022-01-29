c38d3760727b31cf0abbd4f277cef1901e34ba24
zengxuan <zeng.xu@northeastern.edu>
2022-01-28 17:07:56 -0800

    searchbar looks more modern, and pressing Enter now trigers search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 export default function SearchBar() {
     const [searchContent, setSearchContent] = useState('');
     const navigate = useNavigate();
+    const handleEnter = (e) => {
+        if (e.key === 'Enter' && searchContent) {
+            navigate('./searchResult/' + searchContent);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleEnter)
+        return () => { document.removeEventListener('keydown', handleEnter) };
+    });
+
     return (
         <>
             <h1>A REAL JOB BOARD</h1>
@@ -11,10 +22,12 @@ export default function SearchBar() {
                 <input onChange={(e) => {
                     setSearchContent(e.target.value);
                 }} />
-                <button onClick={() => {
+                <span className="material-icons" onClick={() => {
+                    if (!searchContent) return;
                     navigate('./searchResult/' + searchContent);
-                }}>SEARCH</button>
+                }}>search</span>
             </div>
+
             <div id='note'>Dear visitor --  in order to update or delete a job, you have to be its creator.
                 You can try posting a job, or login with username: zengxuan/password: zengxuan to update or delete the job titled 'Sales Engineer'. </div>
         </>
